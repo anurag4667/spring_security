bb7a411db5bd9f594f2b2eeb78921519f6deeb3b
Anurag Rajput <arajput3000@gmail.com>
2025-04-17 01:48:02 +0530

    default

--- src/main/java/com/springsecurity/config/MySecurityConfig.java
+++ src/main/java/com/springsecurity/config/MySecurityConfig.java
@@ -1,23 +1,26 @@
 package com.springsecurity.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
 public class MySecurityConfig  {
 	
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-		http
-		.authorizeRequests()
-		.anyRequest()
-		.authenticated()
-		.and()
-		.httpBasic();
-		return http.build();
-	}
+//	@Bean
+//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        http.csrf(customizer -> customizer.disable()); // DISABLING CSRF TOKEN
+//        http.authorizeHttpRequests(request-> request.anyRequest().authenticated());
+//        http.formLogin(Customizer.withDefaults());
+//        http.httpBasic(Customizer.withDefaults());
+//        return http.build();
+//    }
+//	
+	
+	
 }
