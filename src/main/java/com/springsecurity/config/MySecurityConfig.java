5bdc104ccbd8f747e0696f365c5233cd78d1a7a3
Anurag Rajput <arajput3000@gmail.com>
2025-04-18 00:28:33 +0530

    custom spring security diabled csrf and made program stateless

--- src/main/java/com/springsecurity/config/MySecurityConfig.java
+++ src/main/java/com/springsecurity/config/MySecurityConfig.java
@@ -1,28 +1,30 @@
 package com.springsecurity.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
-
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
+@Configuration
+@EnableWebSecurity
 public class MySecurityConfig  {
 	
-//	@Bean
-//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//
-//        http.csrf(customizer -> customizer.disable()); // DISABLING CSRF TOKEN
-//        http.authorizeHttpRequests(request-> request.anyRequest().authenticated());
-//        http.formLogin(Customizer.withDefaults());
-//        http.httpBasic(Customizer.withDefaults());
-//        return http.build();
-//    }
-//	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+		//session management being stateless means ne sesssion id ill be generated this ill not ork in broser but ill ork ith postman
+		http
+		.csrf(customizer -> customizer.disable())
+		.authorizeHttpRequests(request -> request.anyRequest().authenticated())
+		.formLogin(Customizer.withDefaults())
+        .httpBasic(Customizer.withDefaults())
+		.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        
+        return http.build();
+    }
+	
 	
 }
